8a205a8010fb14e50996c2104dc8b7a046829708
arno <arno@dupere.me>
2022-07-07 11:16:26 +0200

    removed secrets from code + removed outdated smoke test

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -1,17 +1,26 @@
 import { Authenticator } from "remix-auth"
 import { DiscordStrategy, SocialsProvider } from "remix-auth-socials"
 import { sessionStorage } from "~/session.server"
+import invariant from "tiny-invariant"
 
 export let authenticator = new Authenticator(sessionStorage, { sessionKey: '_session' })
 
+invariant(process.env['DISCORD_CLIENT_ID'], 'DISCORD_CLIENT_ID is required')
+invariant(process.env['DISCORD_CLIENT_SECRET'], 'DISCORD_CLIENT_SECRET is required')
+
+let address = 'http://localhost:3000'
+if (process.env.NODE_ENV === "production") {
+  invariant(process.env['FLY_APP_NAME'], 'FLY_APP_NAME is required')
+  address = `https://${process.env['FLY_APP_NAME']}.fly.dev`
+}
+
 authenticator.use(new DiscordStrategy(
   {
-    clientID: '994414124622680104',
-    clientSecret: 'OhEWbDEoJ_ObB7JONYs2uII28z183hi4',
-    callbackURL: `http://localhost:3000/auth/${SocialsProvider.DISCORD}/callback`,
+    clientID: process.env['DISCORD_CLIENT_ID'],
+    clientSecret: process.env['DISCORD_CLIENT_SECRET'],
+    callbackURL: `${address}/auth/${SocialsProvider.DISCORD}/callback`,
     scope: ['identify'],
   }, async ({ profile }) => {
-    console.log(profile)
     return profile
   }
 ))
